3a493261855842c6eeab4013c8d0ab6a64c173ea
Daniel Ecer <de-code@users.noreply.github.com>
2025-06-12 22:35:37 +0100

    Add MCP server using Gradio example (#18)
    * Add MCP server using Gradio example
    
    * linting: blank lines

---
+++ python_examples/ai/mcp/gradio/README.md
@@ -0,0 +1,7 @@
+#  MCP Server using Gradio Examples
+
+The following examples demonstrate creating an MCP server using [Gradio](https://www.gradio.app/main/guides/using-docs-mcp):
+
+- [simple_mcp_server](simple_mcp_server)
+
+Note: these examples focus on the MCP server, rather than Gradio as a whole.
---
+++ python_examples/ai/mcp/gradio/simple_mcp_server/server.py
@@ -0,0 +1,21 @@
+import gradio as gr
+
+
+def add_numbers(a: float, b: float) -> float:
+    """Add two numbers"""
+    return a + b
+
+
+demo = gr.Interface(
+    fn=add_numbers,
+    api_name="add_numbers",
+    inputs=[
+        gr.Number(precision=0, value=1),
+        gr.Number(precision=0, value=2)
+    ],
+    outputs=[gr.Number(precision=0)],
+)
+
+
+if __name__ == "__main__":
+    demo.launch(mcp_server=True)
