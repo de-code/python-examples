fa230676debb622d4717cbd7a58fc41187d781e9
Daniel Ecer <de-code@users.noreply.github.com>
2025-06-18 19:02:12 +0100

    Gradio MCP Server Example

---
+++ python_examples/ai/mcp/gradio/gradio_mcp_server/server.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+import gradio as gr
+from gradio.mcp import GradioMCPServer
+
+
+def add_numbers(a: float, b: float) -> float:
+    """Add two numbers"""
+    return a + b
+
+
+demo = gr.Interface(
+    fn=add_numbers,
+    api_name="add_numbers",
+    inputs=[
+        gr.Number(precision=0, value=1),
+        gr.Number(precision=0, value=2)
+    ],
+    outputs=[gr.Number(precision=0)],
+)
+
+mcp_server = GradioMCPServer(demo)
+
+app = FastAPI(lifespan=mcp_server.lifespan)
+
+mcp_server.launch_mcp_on_sse(
+    app=app,
+    subpath="/mcp",
+    root_path="/dummy_root_path"
+)
