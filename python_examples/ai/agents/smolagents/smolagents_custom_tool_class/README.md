3ae3cedb023472c116cf3e246a68ca9b1805c956
Daniel Ecer <de-code@users.noreply.github.com>
2025-07-02 01:19:42 +0100

    smolagents: added custom tool class example

---
+++ python_examples/ai/agents/smolagents/smolagents_custom_tool_class/README.md
@@ -0,0 +1,29 @@
+# Smolagents Custom Tool Class
+
+In this example we are defining a custom tool via a tool class.
+We are still using [smolagents](https://github.com/huggingface/smolagents) with a chat interfact powered by [Gradio](https://github.com/gradio-app/gradio).
+
+## Environment Variables
+
+| Name | Description | Default |
+| ---- | ----------- | ------- |
+| OPENAI_BASE_URL | The base URL for the OpenAI compatible API | https://api.openai.com/v1 |
+| OPENAI_API_KEY | The API key for the above API | |
+| OPENAI_MODEL_ID | The model to use | gpt-4o-mini |
+
+## Run
+
+```console
+$ uv run -m python_examples.ai.agents.smolagents.smolagents_custom_tool_class.agent
+* Running on local URL:  http://127.0.0.1:7860
+INFO:httpx:HTTP Request: GET http://127.0.0.1:7860/gradio_api/startup-events "HTTP/1.1 200 OK"
+INFO:httpx:HTTP Request: HEAD http://127.0.0.1:7860/ "HTTP/1.1 200 OK"
+* To create a public link, set `share=True` in `launch()`.
+INFO:httpx:HTTP Request: GET https://api.gradio.app/pkg-version "HTTP/1.1 200 OK"
+```
+
+The chat interface is then available via: `http://127.0.0.1:7860`
+
+## Example prompts
+
+- `Can you add 123 and 234?`
---
+++ python_examples/ai/agents/smolagents/smolagents_custom_tool_class/agent.py
@@ -0,0 +1,81 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+
+from pydantic import TypeAdapter
+
+from smolagents import (  # type: ignore[import-untyped]
+    GradioUI,
+    Model,
+    OpenAIServerModel,
+    Tool,
+    ToolCallingAgent
+)
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def get_model() -> Model:
+    return OpenAIServerModel(
+        model_id=os.getenv(
+            'OPENAI_MODEL_ID',
+            'gpt-4o-mini'
+        ),
+        api_base=os.getenv(
+            'OPENAI_BASE_URL',
+            'https://api.openai.com/v1'
+        ),
+        api_key=os.environ['OPENAI_API_KEY'],
+        flatten_messages_as_text=(
+            TypeAdapter(bool).validate_strings(os.getenv(
+                'OPENAI_FLATTEN_MESSAGES',
+                'false'
+            ))
+        )
+    )
+
+
+class AddNumbersTool(Tool):
+    name = "add_numbers"
+    description = "Adds two numbers."
+    inputs = {
+        "a": {
+            "type": "integer",
+            "description": "The first number"
+        },
+        "b": {
+            "type": "integer",
+            "description": "The second number"
+        }
+    }
+    output_type = 'integer'
+
+    def forward(  # pylint: disable=arguments-differ # type: ignore
+        self,
+        a: int,
+        b: int
+    ) -> int:
+        return a + b
+
+
+def main():
+    model = get_model()
+
+    agent = ToolCallingAgent(
+        tools=[AddNumbersTool()],
+        add_base_tools=False,
+        model=model,
+        max_steps=3
+    )
+
+    LOGGER.info("System prompt:\n%s\n", agent.system_prompt)
+
+    GradioUI(agent).launch(share=False)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    load_dotenv()
+    main()
